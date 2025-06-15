15de98cebaa3991cad62771ff764144b2062db58
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-15 17:27:30 -0500

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
 let name = `sіgᥙᥱ ᥱᥣ ᥴᥲᥒᥲᥣ | ᑲ᥆𝗍 ᑲᥲrᑲ᥆zᥲ`
-let imagenes = ["https://files.catbox.moe/ltq7ph.jpg",
-"https://files.catbox.moe/ltq7ph.jpg",
-"https://files.catbox.moe/ltq7ph.jpg",
-"https://files.catbox.moe/ltq7ph.jpg",]
+let imagenes = ["https://d.uguu.se/RuUuoGPE.jpg",
+"https://d.uguu.se/RuUuoGPE.jpg",
+"https://d.uguu.se/RuUuoGPE.jpg",
+"https://d.uguu.se/RuUuoGPE.jpg",]
 
 let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 
