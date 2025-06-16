e9eee0170713050c6131cd063ce740111349633e
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-16 16:29:38 -0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -8,7 +8,7 @@ async function handler(m, { conn }) {
     let name = await conn.getName(ownerJid) || 'Owner'; 
     let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y del The Miku Bot';
     let empresa = 'Adrian- Servicios Tecnológicos';
-    let imagen = 'https://qu.ax/ARhkT.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
+    let imagen = 'https://qu.ax/VGCPX.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
 
     // Enviar imagen junto con el número del dueño y sus detalles
     await conn.sendMessage(m.chat, { 
