83a1e585039f72afd41de7a3595f3bf90dc9c4a8
Wichi <sebastianbarbaro82@gmail.com>
2025-06-16 21:30:40 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -2,13 +2,13 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
-                 "1-Barboza\n" +
+                 "1-Miku\n" +
                  "*✰* https://chat.whatsapp.com/CBuLXuVZcg9FEfCSHiY6b0" +
                  "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
                  "➠ Enlace anulado? entre aquí! \n\n" +
                  "⭐ Canal :\n" +
                  "*✰*https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I" +
-                 "> By Barboza";
+                 "> By Miku bot";
 
     // Asegúrate de definir 'imagen2' correctamente antes de usarlo
     let imagen2 = 'https://qu.ax/Mvhfa.jpg';
