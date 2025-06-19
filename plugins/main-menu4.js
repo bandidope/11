600340e7cf8e02bd812846a320f46b1b84a94ffd
Wichi <sebastianbarbaro82@gmail.com>
2025-06-19 00:50:24 -0400

    Actualizar main-menu4.js

--- plugins/main-menu4.js
+++ plugins/main-menu4.js
@@ -57,7 +57,7 @@ const handler = async (m, { conn}) => {
 🎮 **¡La diversión no tiene límites!**
 `;
 
-    const imageUrl = "https://files.catbox.moe/ltq7ph.jpg";
+        let img = "https://i.ibb.co/LYZrgRs/The-Miku-Bot-MD.jpg";
 
     await conn.sendMessage(m.chat, {
         image: { url: imageUrl},
