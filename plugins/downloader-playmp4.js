1a711f076fc8b9154f9d7ddc1b94460f0e0ebc38
Wichi <sebastianbarbaro82@gmail.com>
2025-06-19 00:13:35 -0400

    Actualizar downloader-playmp4.js

--- plugins/downloader-playmp4.js
+++ plugins/downloader-playmp4.js
@@ -2,24 +2,24 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, text}) => {
-  if (!text) throw m.reply('🌸 Por favor, proporciona una consulta.');
+  if (!text) throw m.reply('⚠️ Por favor, proporciona una consulta.');
 
   const sender = m.sender.split('@')[0];
 
   try {
-    m.reply('🔄🌸 *Procesando tu solicitud...*');
+    m.reply('🔄 *Procesando tu solicitud...*');
 
     const res = await fetch(`https://fastrestapis.fasturl.cloud/downup/ytdown-v1?name=${encodeURIComponent(text)}&format=mp4&quality=720&server=auto`);
     const json = await res.json();
 
     if (!json?.result?.media) {
-      throw new Error('❌🌸 No se encontró el contenido.');
+      throw new Error('❌ No se encontró el contenido.');
 }
 
     const { thumbnail, description, lengthSeconds} = json.result.metadata;
     const { media, title, quality} = json.result;
 
-    const caption = `🎥🌸 *DESCARGA EXITOSA*\n\n📌🌸 *Título:* ${title}\n⏳🌸 *Duración:* ${lengthSeconds} segundos\n🌟🌸 *Calidad:* ${quality}\n\n📄🌸 *Descripción:*\n${description}`;
+    const caption = `🎥 *DESCARGA EXITOSA*\n\n📌 *Título:* ${title}\n⏳ *Duración:* ${lengthSeconds} segundos\n🌟 *Calidad:* ${quality}\n\n📄 *Descripción:*\n${description}`;
 
     // Enviar imagen con información
     await conn.sendMessage(
@@ -39,7 +39,7 @@ let handler = async (m, { conn, args, text}) => {
         video: { url: media},
         mimetype: 'video/mp4',
         fileName: `${title}.mp4`,
-        caption: `✅🌸 *Aquí tienes tu video, @${sender}* 🎬🌸`,
+        caption: `✅ *Aquí tienes tu video, @${sender}* 🎬`,
         mentions: [m.sender]
 },
       { quoted: m}
@@ -47,11 +47,11 @@ let handler = async (m, { conn, args, text}) => {
 
 } catch (e) {
     console.error(e);
-    await conn.sendMessage(m.chat, { text: '⚠️🌸 Intente más tarde, el vídeo es muy pesado o hubo un error al procesarlo.', mentions: [m.sender]}, { quoted: m});
+    await conn.sendMessage(m.chat, { text: '⚠️ Intente más tarde, el vídeo es muy pesado o hubo un error al procesarlo.', mentions: [m.sender]}, { quoted: m});
 }
 };
 
-handler.help = ['play'];
+handler.help = ['play2 <consulta>'];
 handler.tags = ['downloader'];
 handler.command = ["play2"];
 
