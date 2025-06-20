954edefc257bb81cb0bb2294c1359c2ea82037d7
Wichi <sebastianbarbaro82@gmail.com>
2025-06-20 13:38:47 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -55,7 +55,7 @@ const sendAudioWithRetry = async (conn, chat, audioUrl, videoTitle, maxRetries =
           contextInfo: {
             externalAdReply: {
               title: videoTitle,
-              body: "Barboza ",
+              body: "Miku ",
               previewType: 'PHOTO',
               thumbnail: thumbnailBuffer,
               mediaType: 1,
